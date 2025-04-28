1f40e7ac67bf32e2e49a26964096c60308b75196
Jorge Centeno <jcentenome@uoc.edu>
2025-04-28 11:02:59 +0200

    animación pizza

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const temaBtn = document.getElementById('tema-btn');
   const body = document.body;
-
-  // Verifica si el usuario tenía un tema guardado
+ 
   if (localStorage.getItem('tema') === 'oscuro') {
     body.classList.add('tema-oscuro');
   }
 
-  // Evento para cambiar el tema
   temaBtn.addEventListener('click', () => {
     body.classList.toggle('tema-oscuro');
 
-    // Guarda el estado del tema en localStorage
+ 
     if (body.classList.contains('tema-oscuro')) {
       localStorage.setItem('tema', 'oscuro');
     } else {
